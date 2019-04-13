6664c773add74e57564d7cd8e23e8cf7b03fff97
minhchinh97h <minhchinhduong97@gmail.com>
2019-04-14 01:10:01 +0300

    Update procfile

--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -1,3 +1,4 @@
-const serverUrl = "http://10.0.0.6:3001/"
+// const serverUrl = "http://10.0.0.6:3001/"
+const serverUrl = "https://werewolves-of-millers-hollow.herokuapp.com/"
 
 export default serverUrl 
